f1a8b434a5bb5299d136befcfc7743a89e24452c
icweaver <weaveric@gmail.com>
2025-08-22 16:41:00 -0700

    start adding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,25 @@
 using Test
+using TestItemRunner
 
-@testset "add tests" begin
+@testitem "triangle_invariants" begin
+    using Astroalign: triangle_invariants
+
+    points = (
+        (xcenter = 0, ycenter = 0),
+        (xcenter = 0, ycenter = 4),
+        (xcenter = 0, ycenter = -4),
+        (xcenter = 3, ycenter = 0),
+    )
+
+    C, ℳ = triangle_invariants(points)
+
+    combinations = [
+        [(xcenter = 0, ycenter = 0), (xcenter = 0, ycenter =  4), (xcenter = 0, ycenter = -4)],
+        [(xcenter = 0, ycenter = 0), (xcenter = 0, ycenter =  4), (xcenter = 3, ycenter =  0)],
+        [(xcenter = 0, ycenter = 0), (xcenter = 0, ycenter = -4), (xcenter = 3, ycenter =  0)],
+        [(xcenter = 0, ycenter = 4), (xcenter = 0, ycenter = -4), (xcenter = 3, ycenter =  0)],
+    ]
+
+    @test length(C) == 4
+    @test collect(C) == combinations
 end
